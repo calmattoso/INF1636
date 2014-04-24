a6e7cadda9d06d5d36f938a06ee13e22f3b6224c
Carlos Mattoso <calmattoso@hotmail.com>
2014-04-24 13:44:55 -0300

    Major reestructuring of directory structure to conform with industry standards.

---
+++ Monopoly/README.txt
@@ -0,0 +1,4 @@
+INF1636
+=======
+
+Project for the **"Object-Oriented Programming"** course at **PUC-Rio**. It's a version of the game **Monopoly**.
---
+++ Monopoly/src/main/java/com/monopoly/MonopolyApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.monopoly;
+
+/**
+ * @author Carlos Mattoso
+ *
+ */
+public class MonopolyApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
