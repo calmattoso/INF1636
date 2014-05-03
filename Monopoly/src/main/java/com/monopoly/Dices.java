e09c79c9c1d265acc3c106e7a1ef95cedcfa350f
Carlos Mattoso <calmattoso@hotmail.com>
2014-05-03 20:50:18 -0300

    - Updated Dices to allow user specified number of sides. - Now uses Random instead of Math.random().

--- Monopoly/src/main/java/com/monopoly/Dices.java
+++ Monopoly/src/main/java/com/monopoly/Dices.java
@@ -1,16 +1,24 @@
+package com.monopoly;
 
-public class Dice {
+import java.util.Random;
+
+
+public class Dices {
 	private int d1; //Dado numero 1
 	private int d2; //Dado numero 2
+	private int numberOfSides; // Quantidade de lados
+	private Random dice;
 	
-	public Dice(){
+	public Dices( int numberOfSides ){
 		// Inicializa os dados com valores aleatórios
-		roll();
+		dice = new Random();
+		this.numberOfSides = numberOfSides;
 	}
+	
 	public void roll(){
 		// Designa um valor aleatório de 1 a 6 para cada dado
-		d1 = (int) (Math.random()*6) +1;
-		d2 = (int) (Math.random()*6) +1;
+		d1 = dice.nextInt(numberOfSides);
+		d2 = dice.nextInt(numberOfSides);
 	}
 	
 	public int getDie1(){
@@ -22,6 +30,7 @@ public class Dice {
 		// retorna o valor do dado 2
 		return d2;
 	}
+	
 	public int getSum(){
 		// retorna a soma dos valores obtidos nos dados 1 e 2
 		return d1+d2;
