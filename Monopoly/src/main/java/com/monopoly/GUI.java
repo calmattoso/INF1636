6e09ba932e18bffe52cf91f8f4577e57ed31696b
Carlos Mattoso <calmattoso@hotmail.com>
2014-05-03 20:50:44 -0300

    - Major modifications in GUI and Board.

--- Monopoly/src/main/java/com/monopoly/GUI.java
+++ Monopoly/src/main/java/com/monopoly/GUI.java
@@ -3,10 +3,12 @@
  */
 package com.monopoly;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
 
 /**
  * @author Carlos
@@ -14,20 +16,23 @@ import javax.swing.JFrame;
  */
 public class GUI extends JFrame {
 	private static final long serialVersionUID = 1643643618255734885L;
+	private JLayeredPane layers;
 	private String title;	
 	private int width, height;
 	
-	public GUI(String title , Dimension d )
+	public GUI( String title , Dimension d )
 	{
 		this.title = title;
 		
 		this.width  = (int) d.getWidth();
 		this.height = (int) d.getHeight();
+		
+		layers = new JLayeredPane();
 	}
 	
-	public void addComponent( Component component )
+	public void addComponent( Component component , int zIndex , int innerIndex )
 	{
-		add( component );
+		layers.add( component , zIndex , innerIndex );
 	}
 	
 	public void displayWindow( boolean b )
@@ -39,8 +44,10 @@ public class GUI extends JFrame {
 	{
 		 setTitle( this.title );
 		
-		 setVisible( false );
-	     
+		 getContentPane().add( layers , BorderLayout.CENTER );
+		 
+		 setVisible( false );		 
+  
 		 setDefaultCloseOperation( EXIT_ON_CLOSE );
 	     
 		 setSize( this.width , this.height );
@@ -50,8 +57,5 @@ public class GUI extends JFrame {
 	     setResizable( false );
 	  
 	}
-	
-	
-	
 	
 }
--- Monopoly/src/main/java/com/monopoly/MonopolyApp.java
+++ Monopoly/src/main/java/com/monopoly/MonopolyApp.java
@@ -4,6 +4,11 @@
 package com.monopoly;
 
 import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 
 /**
  * @author Carlos Mattoso
@@ -15,12 +20,43 @@ public class MonopolyApp {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Board board = new Board( 10 , 10 );
-		GUI window = new GUI( "Monopoly" , new Dimension( 1024 , 1024 ) );
+		final Board board = new Board( 50 , 50 );
 		
-		window.add( board );
-		window.setupWindow();
+		final Player p1 = new Player( Board.BoardSpaces.START , 0.0 , Player.PlayerColor.BLACK );
+		final PlayerPinView p1View = new PlayerPinView( p1 , new Point( 25 , 25 ));
 		
+		final Dices d = new Dices( 6 );		
+		
+				
+		final GUI window = new GUI( "Monopoly" , new Dimension( 1020 , 1024 ) );
+		
+		p1.addObserver( p1View );
+		
+		JButton changePosition = new JButton("Roll Dice");
+		changePosition.setBounds(0, 0, 130, 20);
+		
+		changePosition.setEnabled(true);
+		changePosition.setActionCommand("change");
+		changePosition.addActionListener( new ActionListener(){
+			public void actionPerformed(ActionEvent arg0){
+				System.out.println("button clicked");
+				
+				d.roll();
+				System.out.println("Dice rolled:" + String.valueOf(d.getDie1()) + " " + String.valueOf(d.getDie2()));
+				
+				Board.BoardSpaces newSpace = p1.getPosition().getNext( d.getSum() );
+				
+				p1.setPosition( newSpace );
+			}
+		});
+		
+		
+		
+		window.addComponent( changePosition , 2 , 0 );
+		window.addComponent( p1View , 1 , 0 );
+		window.addComponent( board , 0 , 0 );		
+				
+		window.setupWindow();		
 		
 		window.displayWindow( true );
 	}
