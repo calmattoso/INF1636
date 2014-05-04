02ff700ac984d4d312f2b6222881e33b2c7989b1
Carlos Mattoso <calmattoso@hotmail.com>
2014-05-04 11:47:09 -0300

    - Updated Dice class, separating behavior into View and Model.

--- Monopoly/src/main/java/com/monopoly/game/Dice.java
+++ Monopoly/src/main/java/com/monopoly/game/Dice.java
@@ -1,15 +1,17 @@
-package com.monopoly;
+package com.monopoly.game;
 
+import java.util.Observable;
 import java.util.Random;
 
-
-public class Dices {
+public class Dice 
+	extends Observable
+{
 	private int d1; //Dado numero 1
 	private int d2; //Dado numero 2
 	private int numberOfSides; // Quantidade de lados
 	private Random dice;
 	
-	public Dices( int numberOfSides ){
+	public Dice( int numberOfSides ){
 		// Inicializa os dados com valores aleatórios
 		dice = new Random();
 		this.numberOfSides = numberOfSides;
@@ -17,10 +19,17 @@ public class Dices {
 	
 	public void roll(){
 		// Designa um valor aleatório de 1 a 6 para cada dado
-		d1 = dice.nextInt(numberOfSides);
-		d2 = dice.nextInt(numberOfSides);
+		d1 = dice.nextInt(numberOfSides) + 1;
+		d2 = dice.nextInt(numberOfSides) + 1;
+		
+		updateObservers();
 	}
 	
+	private void updateObservers() {
+		this.setChanged();		
+		this.notifyObservers( "DICE_dicesRolled" );		
+	}
+
 	public int getDie1(){
 		// retorna o valor do dado 1
 		return d1;
